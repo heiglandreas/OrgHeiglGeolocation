d706a3bd39a7b47cef7a9170b1bbedeec3b124df
Andreas Heigl <andreas@heigl.org>
2013-12-18 10:17:08 +0100

    Adds Module.php

---
+++ Module.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Copyright (c) 2011-2012 Andreas Heigl<andreas@heigl.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @category  MailProxy
+ * @package   OrgHeiglMailproxy
+ * @author    Andreas Heigl<andreas@heigl.org>
+ * @copyright 2011-2012 Andreas Heigl
+ * @license   http://www.opesource.org/licenses/mit-license.php MIT-License
+ * @version   0.0
+ * @since     06.03.2012
+ * @link      http://github.com/heiglandreas/mailproxyModule
+ */
+
+namespace OrgHeiglGeolocation;
+
+/**
+ * The Module-Provider
+ *
+ * @category  Geolocation
+ * @author    Andreas Heigl<andreas@heigl.org>
+ * @copyright 2011-2012 Andreas Heigl
+ * @license   http://www.opesource.org/licenses/mit-license.php MIT-License
+ * @version   0.0
+ * @since     06.03.2012
+ * @link      http://github.com/heiglandreas/HybridAuth
+ */
+class Module
+{
+//    public function getConfig()
+//    {
+//    	return include __DIR__ . '/config/module.config.php';
+//    }
+//
+    public function getAutoloaderConfig()
+    {
+    	return array(
+    			'Zend\Loader\StandardAutoloader' => array(
+    					'namespaces' => array(
+    							__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+    					),
+    			),
+    	);
+    }
+
+    /**
+     * Expected to return \Zend\ServiceManager\Config object or array to
+     * seed such an object.
+     *
+     * @return array|\Zend\ServiceManager\Config
+     */
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'OrgHeiglGeolocation\Options\ModuleOptions' => 'OrgHeiglGeolocation\Service\ModuleOptionsFactory',
+                'OrgHeiglGeolocation\FormManager' => 'OrgHeiglGeolocation\Service\FormManagerFactory',
+                'org_heigl_geolocation.renderer' => 'OrgHeiglGeolocation\Service\RendererFactory',
+            ),
+        );
+    }
+
+    /**
+     * Returns configuration to merge with application configuration
+     *
+     * @return array|\Traversable
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getFormElementConfig()
+    {
+        return array(
+            'invokables' => array(
+                'org_heigl_geolocation.renderer.geolocation' => 'OrgHeiglGeolocation\Renderer\GeolocationRenderer',
+            ),
+        );
+    }
+}
